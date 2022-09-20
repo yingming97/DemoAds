82ae36edbce701ab252c9c0e923a0372d7b85acc
Ming <85300101+yingming97@users.noreply.github.com>
2022-09-20 09:21:11 +0700

    Initial commit

---
+++ app/src/main/java/pham/hien/demoads/MainActivity.kt
@@ -0,0 +1,24 @@
+package pham.hien.demoads
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adView: AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        MobileAds.initialize(this) {}
+
+        adView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+    }
+}
